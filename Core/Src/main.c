eaa6e09f2a3b7393e43bba6c6196b7145afc728d
nadav guy <nadavguy@parazero.com>
2021-11-08 17:05:45 +0200

    Version 1.02 BuildID 1.01 * Re-enabled bootloader deployment

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -216,7 +216,7 @@ int main(void)
   {
     localFlashParams.startAddress = 0x08000000;
     localFlashParams.voltageLevel = FLASH_VOLTAGE_RANGE_3;
-//    reallocateDataFromArray(Array, 0x08000000, bootloaderLength);
+   reallocateDataFromArray(Array, 0x08000000, bootloaderLength);
   }
 
   BSP_QSPI_Init();
